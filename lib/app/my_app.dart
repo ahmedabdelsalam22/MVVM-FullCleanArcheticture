3650fa6b3db125803b4ed0f0f3a2e543117cceb3
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2023-02-06 14:53:54 +0200

    fix some features

---
+++ lib/app/my_app.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_advanced/presentation/resources/theme_manager.dart';
+
+class MyApp extends StatefulWidget {
+  const MyApp._internal();
+
+  static const MyApp _instance = MyApp._internal();
+
+  factory MyApp() => _instance;
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: getApplicationTheme(),
+      home: Container(),
+    );
+  }
+}
