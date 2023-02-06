0ef8aef5c00e836fcbbe42b4a6a9b55e40430882
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2023-02-06 14:07:55 +0200

    create FontSize class font_manager file and add some fonts sizes

--- lib/presentation/resources/font_manager.dart
+++ lib/presentation/resources/font_manager.dart
@@ -11,3 +11,13 @@ class FontWeightManager {
   static const FontWeight semiBold = FontWeight.w500;
   static const FontWeight bold = FontWeight.w700;
 }
+
+class FontSize {
+  static const double s12 = 12.0;
+  static const double s14 = 14.0;
+  static const double s16 = 16.0;
+  static const double s17 = 17.0;
+  static const double s18 = 18.0;
+  static const double s20 = 20.0;
+  static const double s22 = 22.0;
+}
