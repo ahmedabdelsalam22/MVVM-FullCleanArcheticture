981511457c6ef22b055b6648237bc49f245ab351
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2023-02-07 16:26:34 +0200

    create _list which has 4 SliderObject List

--- lib/presentation/onboarding/onboarding_view.dart
+++ lib/presentation/onboarding/onboarding_view.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+import '../resources/assets_manager.dart';
+import '../resources/strings_manager.dart';
+
 class OnBoardingView extends StatefulWidget {
   const OnBoardingView({Key? key}) : super(key: key);
 
@@ -8,6 +11,21 @@ class OnBoardingView extends StatefulWidget {
 }
 
 class _OnBoardingViewState extends State<OnBoardingView> {
+  late final List<SliderObject> _list = _getSliderData();
+
+  static List<SliderObject> _getSliderData() {
+    return [
+      SliderObject(AppStrings.onBoardingTitle1, AppStrings.onBoardingSubTitle1,
+          ImageAssets.onBoardingLogo1),
+      SliderObject(AppStrings.onBoardingTitle2, AppStrings.onBoardingSubTitle2,
+          ImageAssets.onBoardingLogo2),
+      SliderObject(AppStrings.onBoardingTitle3, AppStrings.onBoardingSubTitle3,
+          ImageAssets.onBoardingLogo3),
+      SliderObject(AppStrings.onBoardingTitle4, AppStrings.onBoardingSubTitle4,
+          ImageAssets.onBoardingLogo4),
+    ];
+  }
+
   @override
   Widget build(BuildContext context) {
     return const Scaffold();
