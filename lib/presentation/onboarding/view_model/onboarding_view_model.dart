e02d25e63dd4a70b3eeae10065bb70d3d2005690
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2023-02-09 16:53:20 +0200

    create OnBoardingViewModel class and extends BaseViewModel interface

---
+++ lib/presentation/onboarding/view_model/onboarding_view_model.dart
@@ -0,0 +1,13 @@
+import 'package:flutter_advanced/presentation/base/base_view_model.dart';
+
+class OnBoardingViewModel extends BaseViewModel {
+  @override
+  void dispose() {
+    // TODO: implement dispose
+  }
+
+  @override
+  void start() {
+    // TODO: implement start
+  }
+}
