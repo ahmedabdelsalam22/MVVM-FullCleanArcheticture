d164d24a69a2485e6fe5ad12058d9ed4ffa38c19
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2023-03-21 13:29:02 +0200

    build login method and generated it in app_api.g.dart with buildRunner

--- lib/data/network/app_api.dart
+++ lib/data/network/app_api.dart
@@ -1,11 +1,16 @@
 import 'package:dio/dio.dart';
 import 'package:retrofit/http.dart';
 
-import '../app/constants.dart';
+import '../../app/constants.dart';
+import '../response/responses.dart';
 
 part 'app_api.g.dart';
 
 @RestApi(baseUrl: Constants.baseUrl)
 abstract class AppServiceClient {
   factory AppServiceClient(Dio dio, {String baseUrl}) = _AppServiceClient;
+
+  @POST("/customers/login")
+  Future<AuthResponse> login(
+      @Field("email") String email, @Field("password") String password);
 }
